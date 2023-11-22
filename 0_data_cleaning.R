69bc108736f0f4d652f3926a4887dea42d5cfe2e
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2023-11-22 17:39:57 -0600

    Updates on cleaning and wrangling

--- 0_data_cleaning.R
+++ 0_data_cleaning.R
@@ -7,7 +7,7 @@ library(gt)
 library(webshot2)
 
 # Loading data -----------------
-european_restaurants <- read_csv("data/tripadvisor_european_restaurants.csv")
+data <- read_csv("data/tripadvisor_european_restaurants.csv")
 
 
 # Data management--------------
@@ -33,7 +33,7 @@ head(european_restaurants)
 #  mutate_all(as.integer)
 
 # Dealing with NA value for award and special diets--------------
-restaurants_tidy <- european_restaurants %>%
+data_tidy_1 <- european_restaurants %>%
   rename(
     vegetarian = vegetarian_friendly,
     vegan = vegan_options,
@@ -49,11 +49,11 @@ restaurants_tidy <- european_restaurants %>%
     )) %>%
   select(-vegetarian, -vegan, -gluten_free)  
 
-# Renaming Price level
+# Renaming Price level-----------------
 ## First identifying the types of values given
-unique(restaurants_tidy$price_level)
+unique(data_tidy_1$price_level)
 ## Renaming as Expensive  
-restaurants_tidy <- restaurants_tidy |>
+data_tidy_1 <- data_tidy_1 |>
   mutate(price_level = case_when(
     price_level == "€€€€" ~ "expensive",
     price_level == "€€-€€€" ~ "mid-range",
@@ -63,17 +63,17 @@ restaurants_tidy <- restaurants_tidy |>
 
 # Wrangling top tags and dealing with doubles ------------
 ## Separating the values in the strings
-restaurants_tidy$top_tags <- str_split(restaurants_tidy$top_tags, ", ", simplify = FALSE)
+data_tidy_1$top_tags <- str_split(data_tidy_1$top_tags, ", ", simplify = FALSE)
 
 ## Replace empty strings with NA in each list
-#restaurants_tidy$top_tags <-  ifelse(split_tags == "", NA, split_tags)
-#restaurants_tidy$top_tags <- lapply(restaurants_tidy$top_tags, function(tags) {
+#data_tidy_1$top_tags <-  ifelse(split_tags == "", NA, split_tags)
+#data_tidy_1$top_tags <- lapply(data_tidy_1$top_tags, function(tags) {
 #  tags[tags == ""] <- NA
 #  tags
 #})
 
 ## If you want to collapse the list into a single character vector
-#restaurants_tidy$top_tags <- sapply(restaurants_tidy$top_tags, function(tags) {
+#data_tidy_1$top_tags <- sapply(data_tidy_1$top_tags, function(tags) {
 #  if (!is.na(tags)) {
 #    paste(tags, collapse = ", ")
 #  } else {
@@ -81,15 +81,15 @@ restaurants_tidy$top_tags <- str_split(restaurants_tidy$top_tags, ", ", simplify
 #  }
 #})
 
-#unique(unlist(restaurants_tidy$top_tags))
+#unique(unlist(data_tidy_1$top_tags))
 ## Knowing that Vegetarian, vegan and gluten free has already been dealt
 ##Getting rid of special diet values
-#restaurants_tidy_2 <- restaurants_tidy %>%
+#data_tidy_2 <- data_tidy_1 %>%
 #  filter_at(vars(starts_with("top_tags")), any_vars(!(. %in% c("Vegetarian Friendly",
 #                                                               "Vegan Options", "Gluten Free Options",
 #                                                               "Cheap Eats", "Mid-range", "Fine Dining"))))
 
-restaurants_tidy$top_tags <- lapply(restaurants_tidy$top_tags, function(tags) {
+data_tidy_1$top_tags <- lapply(data_tidy_1$top_tags, function(tags) {
   tags <- tags[!(tags %in% c("Vegetarian Friendly", "Vegan Options", "Gluten Free Options",
                              "Cheap Eats", "Mid-range", "Fine Dining"))]
   if (length(tags) == 0) {
@@ -98,9 +98,8 @@ restaurants_tidy$top_tags <- lapply(restaurants_tidy$top_tags, function(tags) {
   tags
 })
 
-# Reorganizing with price range-------------
-##Creating a new variable: average price
-restaurants_tidy <-  restaurants_tidy |>
+# Creating a new variable: average price-------------
+data_tidy_2 <-  data_tidy_1 |>
  mutate(
   minimum_range = as.numeric(str_extract(price_range, "\\d+(\\.\\d+)?")),
  maximum_range = as.numeric(str_extract_all(price_range, "\\d+(\\.\\d+)?") %>% sapply(function(x) x[2])),
@@ -110,17 +109,16 @@ select(-c(minimum_range, maximum_range))
 
 #Seeing unique values---------------
 ##Meals
-unique(restaurants_tidy$meals)
+unique(data_tidy_2$meals)
 
 ##Cuisines
-unique(restaurants_tidy$cuisines)
+unique(data_tidy_2$cuisines)
 
 ##Unique values of top_tags
-unique(restaurants_tidy$top_tags)
+unique(data_tidy_2$top_tags)
 
-#Showing why I work with cuisines and not top_tags---------------
-# Assuming 'restaurants_tidy' is your dataset
-table_data <- head(select(restaurants_tidy, top_tags, cuisines), 10)
+#Comparison between cuisines and top_tags---------------
+table_data <- head(select(data_tidy_2, top_tags, cuisines), 10)
 
 ## Create a gt table
 gt_table <- gt(table_data) %>%
@@ -133,7 +131,7 @@ gt_table <- gt(table_data) %>%
 gtsave(gt_table, file = "figures/table_image.png")
 
 # Wrangling the top_tags---------------------
-restaurants_tidy_3 <- mutate(restaurants_tidy_2, top_tags = lapply(top_tags, function(tags) {
+data_tidy_2 <- mutate(data_tidy_2, top_tags = lapply(top_tags, function(tags) {
   if (!is.null(tags)) {
     tags[tags %in% c("Pizza", "Northern Italy", "Southern Italy")] <- "Italian"
     tags[tags == "Sushi"] <- "Japanese"
@@ -144,9 +142,38 @@ restaurants_tidy_3 <- mutate(restaurants_tidy_2, top_tags = lapply(top_tags, fun
   return(tags)
 }))
 
+# Dropping variables--------------------
+## After all the wrangling data find the NA for each variable
+rows_NA <- data_tidy_2 |>
+  summarise(across(everything(), ~sum(is.na(.))))
+
+#data_tidy_3 <- select(data_tidy_2, -address, -original_location, 
+#                             -keywords, -features, -cuisines, -popularity_detailed, 
+#                             -popularity_generic, -price_range, -original_open_hours)
+## Seeing Variables with most NA values
+#restaurants_tidy_3[rowSums(is.na(restaurants_tidy_3)) > 35, ]
+#restaurants_tidy_3[rowSums(is.na(restaurants_tidy_3)) > 30, ]
+#restaurants_tidy_3[rowSums(is.na(restaurants_tidy_3)) > 25, ]
+
+
+# Eliminating rows with most NA values--------------
+#restaurants_tidy_4 <- restaurants_tidy_3[rowSums(!is.na(restaurants_tidy_3)) > 24, ]
+#restaurants_tidy_3[rowSums(!is.na(restaurants_tidy_3)) > 24, ]
+
+#rows_NA_2 <- restaurants_tidy_4 |>
+#  summarise(across(everything(), ~sum(is.na(.))))
+
+# Average price-------------------------
+# Decided to prioritize the average_price variable, so got rid of all NA values in it 
+#restaurants_trying <- na.omit(restaurants_tidy_4, cols = "average_price")
+
+#rows_NA_3 <- restaurants_trying |>
+#  summarise(across(everything(), ~sum(is.na(.))))
+#until here
+
 #restaurants_tidy_3 <- restaurants_tidy_3 |>
 #  rename(top_tags = "cuisines")
-  #  mutate(cuisines = trimws(cuisines)) |>
+#  mutate(cuisines = trimws(cuisines)) |>
 #  separate_rows(meals, sep = ", ") |>
 #  mutate(cuisines_present = 1) |>
 #  pivot_wider(names_from = cuisines, values_from = cuisines_present, values_fill = NA)
@@ -155,10 +182,3 @@ restaurants_tidy_3 <- mutate(restaurants_tidy_2, top_tags = lapply(top_tags, fun
 #  unnest(top_tags) %>%
 #  mutate(tag_present = 1) %>%
 #  pivot_wider(names_from = top_tags, values_from = tag_present, values_fill = 0)
-
-# Dropping variables that won't be needed for the analysis----------
-## After all the wrangling data find the NA for each variable
-missingness <- restaurants_tidy_3 |>
-  summarise(across(-avg_price, ~sum(is.na(.))))
-
-#address,original location, keywords, feautures, cuisine, popularity
